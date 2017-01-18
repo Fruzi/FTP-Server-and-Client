f206c85ca3fa27bc0ac015052196fd2da7d8e989
Uzi Friedman <uzifr@post.bgu.ac.il>
2017-01-18 14:42:13 +0000

    Revenge of the ++

--- Server/src/main/java/bgu/spl171/net/srv/Reactor.java
+++ Server/src/main/java/bgu/spl171/net/srv/Reactor.java
@@ -102,13 +102,13 @@ public class Reactor<T> implements Server<T> {
         clientChan.configureBlocking(false);
         BidiMessagingProtocol<T> protocol = protocolFactory.get();
         final NonBlockingConnectionHandler<T> handler = new NonBlockingConnectionHandler<>(
-                connectionId++,
+                connectionId,
                 readerFactory.get(),
                 protocol,
                 clientChan,
                 connections,
                 this);
-        connections.add(connectionId, handler);
+        connections.add(connectionId++, handler);
         pool.submit(handler, () -> protocol.start(connectionId, connections));
         clientChan.register(selector, SelectionKey.OP_READ, handler);
     }
